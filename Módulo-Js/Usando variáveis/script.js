37fb6e45e0cf5c08f077e710e23e64d7c45f8928
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2022-03-01 23:56:36 -0300

    Folder Js started and usando variáveis complete

---
+++ Módulo-Js/Usando variáveis/script.js
@@ -0,0 +1,20 @@
+console.log("Freeza pq vc matou o curirin ?... Eu estou nervoso... Eu estou nervoso... FREEEZAA A A A")
+
+//Variáveis Padrão 
+let meuNumero = 20
+let outroNumero = 21 
+
+//Printando uma soma simples
+console.log(meuNumero+outroNumero)
+//Printando uma frase em cima de uma simples soma
+console.log(`O resultado é ${meuNumero+outroNumero}`)
+
+//Variavel constante, não pode redefinir ela
+const mesmoNumero = 20
+console.log(`O valor n vai mudar TROXA ele é ${mesmoNumero} PORRA`)
+
+ const mesmoNumero2 = 30
+
+console.log(`${mesmoNumero2} TIVE QUE METER OUTRA CONST PQ SE N KJJKJKJK A PORRA IA BUGAR JKJKJKJKJKJK`)
+
+
