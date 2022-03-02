96203345b7077562cb021e875486ff9833d1ffa0
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2022-03-02 16:00:00 -0300

    Arrays complete

---
+++ Módulo-Js/Arrays/script.js
@@ -0,0 +1,26 @@
+let predio = [
+  'Terreo',
+  'Priemiro Andar',
+  'Segundo Andar',
+  'Terceiro Andar',
+  'Quarto andar',
+]
+
+console.log(predio[3])
+console.log(predio)
+
+//Adiciona um novo item na ultima posição do Array
+predio.push('Quinto Andar')
+console.log(predio)
+
+//Tira a ultima posição do Array 
+predio.pop()
+console.log(predio)
+
+//Adiciona na 1 prosição do Array
+predio.unshift('Subsolo')
+console.log(predio)
+
+//Retira na 1 posição do Array
+predio.shift()
+console.log(predio)
