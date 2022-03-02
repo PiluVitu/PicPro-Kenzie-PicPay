0122b79629109669679d9fc936b9e2276ee81817
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2022-03-02 02:05:05 -0300

    Introdução às Condições (if/else) complete

---
+++ Módulo-Js/Introdução-às-Condições-(if-else)/script.js
@@ -0,0 +1,12 @@
+// If = Se; If Else = Mas se; Else = Se não
+
+let x = 5.8
+
+if(x === 5.9){
+  console.log('KRl kkjkkkjkj NO detalhe man ')
+}else if(x <= 6){
+  console.log('Ihhhhh boy, foi de base')
+}else {
+console.log('Passou')
+}
+
