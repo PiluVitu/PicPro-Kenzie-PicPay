866c840fa804c01a8a675de78b4a215014df21ba
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2022-03-02 17:44:21 -0300

    Estruturas de repetição complete and starterd Objetos em Javascript

---
+++ Módulo-Js/Estruturas de repetição/script.js
@@ -0,0 +1,25 @@
+/*
+for (inicial; condição; incremento){
+  /instrução
+}
+*/
+
+//Repetição para add até 3 
+for (let contador = 0; contador < 3; contador++){
+  console.log('Kenzie')
+}
+
+//Contador paqra subitrair até 0 
+
+for (let contadormen = 10; contadormen > 0; contadormen -= 1){
+  console.log('Kenzizada')
+}
+
+//Array de estados
+let states =  ['Piauí', 'Bahia','São Paulo', 'Brasilia', 'Fortaleza' ]
+
+
+//Laço de repetição para printar os estados dentro de um Array 
+for (let i=0; i < states.length; i++) {
+  console.log(states[i])
+}
